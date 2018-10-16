144264f919c4884df131751a83884df449921b89
Richard Green <richard.green@blocksure.com>
2018-10-16 16:09:58 +0100

    Now understands drag and drop file transfer

--- src/main/kotlin/net/corda/chat/Chat.kt
+++ src/main/kotlin/net/corda/chat/Chat.kt
@@ -5,9 +5,12 @@ import com.github.ricksbrown.cowsay.Cowsay
 import javafx.application.Application
 import javafx.beans.Observable
 import javafx.event.ActionEvent
+import javafx.event.EventHandler
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.control.*
+import javafx.scene.input.DragEvent
+import javafx.scene.input.TransferMode
 import javafx.stage.Stage
 import net.corda.client.jfx.utils.observeOnFXThread
 import net.corda.client.rpc.CordaRPCClient
@@ -30,9 +33,15 @@ import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.NetworkHostAndPort
 import net.corda.core.utilities.ProgressTracker
 import org.apache.activemq.artemis.api.core.ActiveMQNotConnectedException
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.security.PublicKey
 import java.time.Instant
 import java.time.ZoneId
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 import kotlin.system.exitProcess
 
 class Chat : Contract {
@@ -77,12 +86,12 @@ class ChatApp : Application() {
     private var rpc: CordaRPCOps? = null
 
     private val host: String
-        get() = if (parameters.unnamed.size > 0) parameters.unnamed[0] else "localhost:10006"
+        get() = if (parameters.unnamed.size > 0) parameters.unnamed[0] else "localhost:10008"
 
     // Inside Graviton, this runs whilst the spinner animation is active, so we can be kinda slow here.
     override fun init() {
         try {
-            rpc = CordaRPCClient(NetworkHostAndPort.parse(host)).start("guest", "letmein").proxy
+            rpc = CordaRPCClient(NetworkHostAndPort.parse(host)).start("user1", "test").proxy
         } catch (e: ActiveMQNotConnectedException) {
             // Delay showing the error message until we're inside start(), when JavaFX is started up fully.
         }
@@ -139,6 +148,21 @@ class ChatUIController {
         }
     }
 
+    private fun sendFile(file: Path) {
+        val fo = File.createTempFile("/tmp",".corda.zip")
+        println(" --> tmp file is $fo")
+
+        ZipOutputStream(fo.outputStream()).use { zos ->
+            zos.putNextEntry(ZipEntry(file.fileName.toString()))
+            Files.newInputStream(file).use {
+                it.copyTo(zos)
+            }
+        }
+
+        val attachmentHash = rpc.uploadAttachment(fo.inputStream())
+        println("File uploaded as $attachmentHash")
+        textArea.text += "File $file sent as hash $attachmentHash\n"
+    }
     fun onMoo(event: ActionEvent) {
         val m = if (messageEdit.text.isBlank()) "moo" else messageEdit.text
         send(":cow:$m")
@@ -154,7 +178,21 @@ class ChatUIController {
                 .filterNot { it.isNotary }
         identitiesList.items.addAll(names)
         identitiesList.selectionModel.selectedItems.addListener { _: Observable ->
-            messageEdit.promptText = "Type message to ${identitiesList.selectionModel.selectedItems.first().party.name.organisation} here"
+            messageEdit.promptText = "Type message to ${identitiesList.selectionModel.selectedItems.first().party.name.organisation} here or drag and drop a file"
+            messageEdit.setOnDragOver {
+                if (it.gestureSource != messageEdit && it.dragboard.hasFiles()) {
+                    it.acceptTransferModes(TransferMode.MOVE, TransferMode.COPY);
+                }
+                it.consume()
+            }
+            messageEdit.setOnDragDropped{
+                val db = it.dragboard
+                if (db.hasFiles()) {
+                    db.files.forEach {
+                        sendFile(Paths.get(it.path))
+                    }
+                }
+            }
         }
         identitiesList.selectionModel.select(0)
         updates.observeOnFXThread().subscribe {
